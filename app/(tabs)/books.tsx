60b63703c3714eaddad7871f0d195ed7a03108f3
x <x@xxxxxxx.dev>
2025-02-15 23:54:28 +0100

    Set up Tamagui

---
+++ app/(tabs)/books.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "tamagui"
+
+export default function Books() {
+  return (
+    <View>
+      <Text>Books</Text>
+    </View>
+  )
+}
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Button, Text, View } from "tamagui"
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hey</Text>
+      <Button>Click</Button>
+    </View>
+  )
+}
