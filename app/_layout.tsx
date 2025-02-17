92e61593e6e4b4c65355833c79abda6c8b0ae2ee
x <x@xxxxxxx.dev>
2025-02-17 17:48:14 +0100

    Remove RNR due to bugged CLI and use pure NativeWind with manual component copy/paste when needed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,54 +1,16 @@
-import "~/global.css"
+import "@/global.css"
 
-import { DarkTheme, DefaultTheme, Theme, ThemeProvider } from "@react-navigation/native"
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native"
 import { Stack } from "expo-router"
-import { StatusBar } from "expo-status-bar"
+import { useColorScheme } from "nativewind"
 import * as React from "react"
-import { Platform } from "react-native"
-import { navTheme } from "~/lib/constants"
-import { useColorScheme } from "~/lib/useColorScheme"
-
-const lightTheme: Theme = {
-  ...DefaultTheme,
-  colors: navTheme.light,
-}
-const darkTheme: Theme = {
-  ...DarkTheme,
-  colors: navTheme.dark,
-}
-
-// Catch any errors thrown by the Layout component.
-export { ErrorBoundary } from "expo-router"
 
 export default function RootLayout() {
-  const hasMounted = React.useRef(false)
-  const { isDarkColorScheme } = useColorScheme()
-  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false)
-
-  useIsomorphicLayoutEffect(() => {
-    if (hasMounted.current) {
-      return
-    }
-
-    if (Platform.OS === "web") {
-      // Add the background color to the html element to prevent white background on overscroll
-      document.documentElement.classList.add("bg-background")
-    }
-    setIsColorSchemeLoaded(true)
-    hasMounted.current = true
-  }, [])
-
-  if (!isColorSchemeLoaded) {
-    return null
-  }
+  const { colorScheme } = useColorScheme()
 
   return (
-    <ThemeProvider value={isDarkColorScheme ? darkTheme : lightTheme}>
-      <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack />
     </ThemeProvider>
   )
 }
-
-const useIsomorphicLayoutEffect =
-  Platform.OS === "web" && typeof window === "undefined" ? React.useEffect : React.useLayoutEffect
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,13 @@
-import { StatusBar } from "expo-status-bar"
-import { View } from "react-native"
-import { Text } from "~/components/ui/text"
-import "~/global.css"
+import React, { useState } from "react"
+import { Switch, Text, View } from "react-native"
 
 export default function Index() {
+  const [checked, setChecked] = useState(false)
+
   return (
     <View className="flex-1 items-center justify-center">
-      <Text>Toad Reader</Text>
       <Text className="text-green-500">Toad Reader</Text>
-      <StatusBar style="auto" />
+      <Switch onValueChange={setChecked} value={checked} />
     </View>
   )
 }
