2952de4cde55c69d1022a60d2cc247a102717953
x <x@xxxxxxx.dev>
2025-02-20 15:25:42 +0100

    Update deps and setup Supabase

---
+++ lib/supabase.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { createClient } from "@supabase/supabase-js"
+import "react-native-url-polyfill/auto"
+
+const supabaseUrl = "https://<TODO>.supabase.co"
+const supabaseAnonKey = "<TODO>"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
